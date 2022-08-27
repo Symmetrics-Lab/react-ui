2fef53b72b67909f603258b43a4c4d1191e4e7c0
Olivers De Abreu <oliversd@gmail.com>
2022-08-27 18:32:41 -0400

    feat: Button size and disabled

--- docs/src/App.tsx
+++ docs/src/App.tsx
@@ -1,10 +1,10 @@
-import { Button } from "@symlab/components";
+import { Button } from '@symlab/components';
 
 function App() {
   return (
-    <div>
+    <div className="p-4">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <Button text="Click me" />
+      <Button text="Click me" size="md" disabled onClick={() => console.log('click')} />
     </div>
   );
 }
