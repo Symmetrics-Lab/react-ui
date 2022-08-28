9798ad156a5c1674e31362d32c507237c65e64f5
Olivers De Abreu <oliversd@gmail.com>
2022-08-27 22:33:01 -0400

    feat: Button variations and icon

--- docs/src/App.tsx
+++ docs/src/App.tsx
@@ -1,10 +1,17 @@
 import { Button } from '@symlab/components';
+import { EnvelopeIcon } from '@heroicons/react/20/solid';
 
 function App() {
   return (
     <div className="p-4">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <Button text="Click me" size="md" disabled onClick={() => console.log('click')} />
+      <Button
+        text="Click me"
+        size="xl"
+        onClick={() => console.log('click')}
+        icon={EnvelopeIcon}
+        iconPosition="left"
+      />
     </div>
   );
 }
--- docs/tailwind.config.cjs
+++ docs/tailwind.config.cjs
@@ -1,12 +1,31 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-    "../lib/dist/**/*.{vue,js,ts,jsx,tsx}",
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+    '../lib/dist/**/*.{vue,js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: colors.purple[50],
+          100: colors.purple[100],
+          200: colors.purple[200],
+          300: colors.purple[300],
+          400: colors.purple[400],
+          500: colors.purple[500],
+          600: colors.purple[600],
+          700: colors.purple[700],
+          800: colors.purple[800],
+          900: colors.purple[900],
+        },
+        primaryText: colors.white,
+      },
+    },
   },
   plugins: [],
 };
