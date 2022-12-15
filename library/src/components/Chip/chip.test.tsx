5e4f6f11d4ad9c9f1e4350229374ca69a25f9563
jnievesio <joselyne@symlab.io>
2022-12-15 09:20:41 -0500

    feat(chip): add test

---
+++ library/src/components/Chip/chip.test.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import { render, screen } from '@testing-library/react';
+import { test, describe, expect } from 'vitest';
+import '@testing-library/jest-dom/extend-expect';
+import Chip from './Chip';
+
+describe('Chip', () => {
+  test('render Chip', () => {
+    render(
+      <Chip key="default" data-testid="size" variant="default">
+        Entre 8 y 15 caracteres
+      </Chip>
+    );
+
+    const chip = screen.getByTestId('size');
+    expect(chip).toBeDefined();
+  });
+  test('the chip allows to change the background color', () => {
+    render(
+      <Chip
+        key="secondary"
+        variant="default"
+        data-testid="size"
+        className="bg-symlab-purple-500 text-white"
+      >
+        Entre 8 y 15 caracteres
+      </Chip>
+    );
+    const chip = screen.getByTestId('size');
+    expect(chip.className).contain('bg-symlab-purple-500');
+  });
+});
