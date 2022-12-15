574fa6d92fe1cd8fe29345a2605af1b409e16d42
jnievesio <joselyne@symlab.io>
2022-12-15 09:32:05 -0500

    feat(switch): add test

---
+++ library/src/components/Switch/switch.test.tsx
@@ -0,0 +1,53 @@
+import { render, screen } from '@testing-library/react';
+import { test, describe, expect } from 'vitest';
+import '@testing-library/jest-dom/extend-expect';
+import Switch from './Switch';
+
+describe('Switch', () => {
+  test('render Switch', () => {
+    render(
+      <Switch
+        id="primary"
+        variant="primary"
+        size="sm"
+        toggle={true}
+        data-testid="switch"
+        onClick={() => false}
+      ></Switch>
+    );
+    const switchs = screen.getByTestId('switch');
+    expect(switchs).toBeDefined();
+  });
+  test('Switch is disable', () => {
+    render(
+      <Switch
+        id="disabled"
+        variant="primary"
+        size="sm"
+        toggle={true}
+        disabled
+        data-testid="switch"
+        onClick={() => false}
+      ></Switch>
+    );
+    const switchs = screen.getByTestId('switch');
+    expect(switchs.getAttribute('disabled'));
+  });
+  test('the switch allows to change the background color', () => {
+    render(
+      <Switch
+      id="customized"
+      variant="primary"
+      size="sm"
+      toggle={true}
+      data-testid="switch"
+      onClick={() => false}
+        className={'bg-red-500 text-red-600 '}
+      >
+        Entre 8 y 15 caracteres
+      </Switch>
+    );
+    const switchs = screen.getByTestId('switch');
+    expect(switchs.className).contain('bg-red-500');
+  });
+});
