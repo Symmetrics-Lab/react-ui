b1206edc6f9c296ea418409fec51055be05113a5
jnievesio <joselyne@symlab.io>
2023-03-13 11:30:34 -0300

    feat: add project version

---
+++ docs/vite-env.d.ts
@@ -0,0 +1,9 @@
+import 'vite/client';
+interface ImportMetaEnv {
+    readonly PACKAGE_VERSION: string;
+    // more env variables...
+}
+  
+interface ImportMeta {
+    readonly env: ImportMetaEnv
+}
