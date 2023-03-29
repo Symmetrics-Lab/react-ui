35c916d509ecc9d516f79b86b654f876770ed0e6
jnievesio <joselyne@symlab.io>
2023-03-29 12:04:18 -0300

    feat(docs): add notification section

--- docs/src/components/Navbar.tsx
+++ docs/src/components/Navbar.tsx
@@ -3,73 +3,72 @@ import { Link, useLocation } from 'react-router-dom';
 import {
   BellIcon,
   UserIcon,
-} from '@heroicons/react/20/solid';
-import { Dropdown, DropdownItem, Logo, NavBar, TopMenu } from '@symlab/react-ui';
+} from '@heroicons/react/20/solid';import { Dropdown, DropdownItem, Logo, NavBar, TopMenu } from '@symlab/react-ui';
 import { TopMenuItemProps } from '@symlab/react-ui/dist/navigation/TopMenuItem/TopMenuItem.types';
 import clsx from 'clsx';
 import { forwardRef } from 'react';
-
+import Notifications from './sections/Notifications';
 
 const items = [
+  {
+    label: 'Home',
+    link: '/',
+  },
+  {
+    label: 'Forms',
+    link: '/forms',
+  },
+  {
+    label: 'Palette',
+    link: '/palette',
+  },
+  {
+    label: 'Contact',
+    link: '/contact',
+  },
+  {
+    label: 'Login',
+    link: '/login',
+  },
+  {
+    label: 'Register',
+    link: '/register',
+  },
+];
+
+const dropItems = [
+  {
+    label: 'Profile',
+    link: '/profile',
+  },
+  {
+    label: 'Settings',
+    link: '/settings',
+  },
+  {
+    label: 'Sign out',
+    link: '/signout',
+  },
+];
+
+const LogoElement = () => (
+  <Logo src="https://symlab.io/foot-icon.svg" alt="SymLab" href="/" title="Go to home page" />
+);
+
+const LinkComp = (props: TopMenuItemProps) => {
+  const classes = clsx(
+    'sym-top-menu-item inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-sym-header-txt dark:text-sym-header-txt-dark hover:border-sym-border dark:hover:border-sym-border-dark',
     {
-      label: 'Home',
-      link: '/',
-    },
-    {
-      label: 'Forms',
-      link: '/forms',
-    },
-    {
-      label: 'Palette',
-      link: '/palette',
-    },
-    {
-      label: 'Contact',
-      link: '/contact',
-    },
-    {
-      label: 'Login',
-      link: '/login',
-    },
-    {
-      label: 'Register',
-      link: '/register',
-    },
-  ];
-  
-  const dropItems = [
-    {
-      label: 'Profile',
-      link: '/profile',
-    },
-    {
-      label: 'Settings',
-      link: '/settings',
-    },
-    {
-      label: 'Sign out',
-      link: '/signout',
-    },
-  ];
-  
-  const LogoElement = () => (
-    <Logo src="https://symlab.io/foot-icon.svg" alt="SymLab" href="/" title="Go to home page" />
+      'sym-top-menu-item__current border-sym-primary dark:border-sym-primary-dark text-sym-primary dark:text-sym-primary-dark':
+        props.current,
+    }
   );
-  
-  const LinkComp = (props: TopMenuItemProps) => {
-    const classes = clsx(
-      'sym-top-menu-item inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-sym-header-txt dark:text-sym-header-txt-dark hover:border-sym-border dark:hover:border-sym-border-dark',
-      {
-        'sym-top-menu-item__current border-sym-primary dark:border-sym-primary-dark text-sym-primary dark:text-sym-primary-dark': props.current,
-      }
-    );
-    return (
-      <Link to={props.link} role="menuitem" className={classes}>
-        {props.label}
-      </Link>
-    );
-  };
-  
+  return (
+    <Link to={props.link} role="menuitem" className={classes}>
+      {props.label}
+    </Link>
+  );
+};
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const DropItem = forwardRef<HTMLAnchorElement, any>(function DropItem(props, ref) {
@@ -89,13 +88,18 @@ export default function Navbar(): JSX.Element {
         leftItems={<TopMenu items={items} as={LinkComp} currentPath={pathname} />}
         rightItems={
           <>
-            <button
-              type="button"
-              className="rounded-full p-1 text-sym-header-txt dark:text-sym-header-txt-dark focus:outline-none focus:ring-2 focus:ring-sym-primary dark:focus:ring-sym-primary-dark "
+            <Dropdown
+              srLabel="Open notifications"
+              className=""
+              subClassName={
+                'w-[440px] border-2 dark:border-sym-border-dark border-sym-border overflow-auto'
+              }
+              icon={
+                <BellIcon className="items-center text-sym-header-txt dark:text-sym-header-txt-dark" />
+              }
             >
-              <span className="sr-only">View notifications</span>
-              <BellIcon className="h-6 w-6" aria-hidden="true" />
-            </button>
+              <Notifications />
+            </Dropdown>
             <Dropdown srLabel="Open Profile" icon={<UserIcon className="text-sym-header-txt dark:text-sym-header-txt-dark h-8 w-8" />}>
               {dropItems.map((item) => (
                 <DropdownItem
